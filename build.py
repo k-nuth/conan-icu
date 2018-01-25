3461206c30003495b154b03291a9d22c501cf638
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-01-25 17:00:52 -0300

    fix

--- build.py
+++ build.py
@@ -2,12 +2,12 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
-    builder.add_common_builds(shared_option_name="conan-icu:shared")
+    builder.add_common_builds(shared_option_name="icu:shared")
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
-                and not options["conan-icu:shared"]:
+                and not options["icu:shared"]:
 
             filtered_builds.append([settings, options, env_vars, build_requires])
 
