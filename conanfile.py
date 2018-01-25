a848ba25ee72431ed8d08243619d15750a0b6a87
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-01-25 16:52:19 -0300

    changes for bitprim

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 #
-# Changelog
+# Copyright (c) 2017 Bitprim developers (see AUTHORS)
 #
-# v60.2
+# This file is part of Bitprim.
 #
-# - Incomplete data of ICU fixed upstream
-#   Ticket http://bugs.icu-project.org/trac/ticket/13139
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
@@ -34,7 +40,7 @@ class IcuConan(ConanFile):
                "with_unit_tests": [True, False],
                "silent": [True, False]}
 
-    default_options = "shared=True", \
+    default_options = "shared=False", \
                       "data_packaging=archive", \
                       "with_unit_tests=False", \
                       "silent=True"
