d9e4c82e266e2a883606f1d2f1715249f53cb508
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-02-05 16:31:05 -0300

    pure_c=False, it is a C++ package

--- build.py
+++ build.py
@@ -2,12 +2,17 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
-    builder.add_common_builds(shared_option_name="icu:shared")
+    builder.add_common_builds(shared_option_name="icu:shared", pure_c=False)
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
+
+        print(settings)
+        
         if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
                 and not options["icu:shared"]:
+
+                # and not ("mpir:shared" in options and options["mpir:shared"]):
 
             filtered_builds.append([settings, options, env_vars, build_requires])
 
