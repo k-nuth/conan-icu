debf204b52c6ceefc2bbcb27ebe36894d95dcf04
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-01-28 21:35:42 -0300

    fix CI scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,7 +19,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-pip install conan --upgrade
+# pip install conan --upgrade
 pip install conan_package_tools
+pip install kthbuild --upgrade
 
 conan user
